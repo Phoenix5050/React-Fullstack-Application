8a7324ad500a19d560ef3bfd125b56603a60e38d
Phoenix5050 <59894902+Phoenix5050@users.noreply.github.com>
2025-04-06 13:42:46 -0400

    Created API for comment section, added navbar CSS

--- server/index.js
+++ server/index.js
@@ -5,14 +5,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = require('./models');
+const db = require("./models");
 
 // Routers
-const postRouter = require('./routes/Posts');
+const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
 
 db.sequelize.sync().then(() => {
-    app.listen(3001, () => {
-        console.log("Server running on port 3001");
-    });
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
 });
