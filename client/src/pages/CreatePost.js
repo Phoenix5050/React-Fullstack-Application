e0f951856815e6935e5626cebca4fa647d3ab7b3
Phoenix5050 <59894902+Phoenix5050@users.noreply.github.com>
2025-04-05 21:58:31 -0400

    Added posting page and post button, connected to backend

---
+++ client/src/pages/CreatePost.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+function CreatePost() {
+  const initialValues = {
+    title: "",
+    postText: "",
+    userName: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("You must input a title!"),
+    postText: Yup.string().required("You must input a post!"),
+    userName: Yup.string()
+      .min(3, "Username must be at least 3 characters")
+      .max(15, "Username must be at most 15 characters")
+      .required("You must input a username!"),
+  });
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/posts", data).then((response) => {
+      console.log("It Worked!");
+    });
+  };
+
+  return (
+    <div className="createPostPage">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Title: </label>
+          <ErrorMessage name="title" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="title"
+            placeholder="(Ex. Title...)"
+          />
+          <label>Post: </label>
+          <ErrorMessage name="posttext" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="postText"
+            placeholder="(Ex. Post...)"
+          />
+          <label>userName: </label>
+          <ErrorMessage name="userName" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="userName"
+            placeholder="(Ex. John...)"
+          />
+
+          <button type="submit">Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+}
+
+export default CreatePost;
